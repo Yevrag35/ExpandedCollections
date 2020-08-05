f7e64c745eeb1006bff98372449a47117b36b0bc
Mike Garvey <mike.garvey@yevrag35.com>
2020-08-05 16:50:31 -0400

    Add project files.

---
+++ MG.Collections/ReadOnlyList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace MG.Collections
+{
+    public class ReadOnlyList<T> : IReadOnlyList<T>, ICollection
+    {
+        protected List<T> InnerList { get; }
+
+        public int Count => this.InnerList.Count;
+
+        bool ICollection.IsSynchronized => ((ICollection)this.InnerList).IsSynchronized;
+        object ICollection.SyncRoot => ((ICollection)this.InnerList).SyncRoot;
+
+        public T this[int index] => this.InnerList[index];
+
+        protected ReadOnlyList() => this.InnerList = new List<T>();
+        protected ReadOnlyList(int capacity) => this.InnerList = new List<T>(capacity);
+        public ReadOnlyList(IEnumerable<T> items) => this.InnerList = new List<T>(items);
+
+        public void CopyTo(Array array, int index) => ((ICollection)this.InnerList).CopyTo(array, index);
+        public IEnumerator<T> GetEnumerator() => this.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        public int BinarySearch(T item) => this.InnerList.BinarySearch(item);
+        public bool Contains(T item) => this.InnerList.Contains(item);
+        public IList<TOutput> ConvertAll<TOutput>(Converter<T, TOutput> converter) => this.InnerList.ConvertAll(converter);
+        public bool Exists(Predicate<T> match) => this.InnerList.Exists(match);
+        public T Find(Predicate<T> match) => this.InnerList.Find(match);
+        public IList<T> FindAll(Predicate<T> match) => this.InnerList.FindAll(match);
+        public int FindIndex(Predicate<T> match) => this.InnerList.FindIndex(match);
+        public int FindIndex(int startIndex, Predicate<T> match) => this.InnerList.FindIndex(startIndex, match);
+        public int FindIndex(int startIndex, int count, Predicate<T> match) => this.InnerList.FindIndex(startIndex, count, match);
+        public T FindLast(Predicate<T> match) => this.InnerList.FindLast(match);
+        public int FindLastIndex(Predicate<T> match) => this.InnerList.FindLastIndex(match);
+        public int FindLastIndex(int startIndex, Predicate<T> match) => this.InnerList.FindLastIndex(startIndex, match);
+        public int FindLastIndex(int startIndex, int count, Predicate<T> match) => this.InnerList.FindLastIndex(startIndex, count, match);
+        public IList<T> GetRange(int index, int count) => this.InnerList.GetRange(index, count);
+        public int IndexOf(T item) => this.InnerList.IndexOf(item);
+        public int IndexOf(T item, int index) => this.InnerList.IndexOf(item, index);
+        public int IndexOf(T item, int index, int count) => this.InnerList.IndexOf(item, index, count);
+        public int LastIndexOf(T item) => this.InnerList.LastIndexOf(item);
+        public int LastIndexOf(T item, int index) => this.InnerList.LastIndexOf(item, index);
+        public int LastIndexOf(T item, int index, int count) => this.InnerList.LastIndexOf(item, index, count);
+        public void Reverse() => this.InnerList.Reverse();
+        public void Reverse(int index, int count) => this.InnerList.Reverse(index, count);
+        public void Sort() => this.InnerList.Sort();
+        public void Sort(IComparer<T> comparer) => this.InnerList.Sort(comparer);
+        public T[] ToArray() => this.InnerList.ToArray();
+        public bool TrueForAll(Predicate<T> match) => this.InnerList.TrueForAll(match);
+    }
+}
---
+++ MG.Collections/StringKeyedDictionary.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MG.Collections
+{
+    public class StringKeyedDictionary<T> : IDictionary<string, T>
+    {
+        private Dictionary<string, T> _dict;
+
+        public ICollection<string> Keys => _dict.Keys;
+        public ICollection<T> Values => _dict.Values;
+
+        public int Count => _dict.Count;
+        bool ICollection<KeyValuePair<string, T>>.IsReadOnly => ((ICollection<KeyValuePair<string, T>>)_dict).IsReadOnly;
+
+        public T this[string key]
+        {
+            get => _dict[key];
+            set => _dict[key] = value;
+        }
+
+        public StringKeyedDictionary(bool ignoreCase, int capacity)
+        {
+            IEqualityComparer<string> comparer = null;
+            if (ignoreCase)
+                comparer = new IgnoreCase();
+
+            _dict = new Dictionary<string, T>(capacity, comparer);
+        }
+        public StringKeyedDictionary(int capacity, IEqualityComparer<string> equalityComparer)
+        {
+            _dict = new Dictionary<string, T>(capacity, equalityComparer);
+        }
+        public StringKeyedDictionary(IDictionary dictionary, IEqualityComparer<string> comparer)
+        {
+            _dict = new Dictionary<string, T>(dictionary.Count, comparer);
+            dictionary.Keys.OfType<string>().Distinct
+        }
+
+        #region EQUALITY COMPARERS
+        private class IgnoreCase : IEqualityComparer<string>
+        {
+            private StringComparison _comparison;
+
+            public IgnoreCase(StringComparison comparison = StringComparison.CurrentCultureIgnoreCase)
+            {
+                _comparison = comparison;
+            }
+
+            public bool Equals(string x, string y)
+            {
+                if (x == null && y == null)
+                    return true;
+
+                else if ((x == null && y != null) || (x != null && y == null))
+                    return false;
+
+                else
+                    return x.Equals(y, _comparison);
+            }
+            public int GetHashCode(string x)
+            {
+                if (_comparison == StringComparison.CurrentCultureIgnoreCase)
+                    return x.ToLower().GetHashCode();
+
+                else
+                    return x.GetHashCode();
+            }
+        }
+
+        #endregion
+
+        #region METHODS
+        public void Add(string key, T value) => _dict.Add(key, value);
+        public bool ContainsKey(string key) => _dict.ContainsKey(key);
+        public bool Remove(string key) => _dict.Remove(key);
+        public bool TryGetValue(string key, out T value) => _dict.TryGetValue(key, out value);
+        void ICollection<KeyValuePair<string, T>>.Add(KeyValuePair<string, T> item) => ((ICollection<KeyValuePair<string, T>>)_dict).Add(item);
+        public void Clear() => _dict.Clear();
+        bool ICollection<KeyValuePair<string, T>>.Contains(KeyValuePair<string, T> item) => ((ICollection<KeyValuePair<string, T>>)_dict).Contains(item);
+        void ICollection<KeyValuePair<string, T>>.CopyTo(KeyValuePair<string, T>[] array, int arrayIndex) => ((ICollection<KeyValuePair<string, T>>)_dict).CopyTo(array, arrayIndex);
+        bool ICollection<KeyValuePair<string, T>>.Remove(KeyValuePair<string, T> item) => ((ICollection<KeyValuePair<string, T>>)_dict).Remove(item);
+        public IEnumerator<KeyValuePair<string, T>> GetEnumerator() => _dict.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => _dict.GetEnumerator();
+
+        #endregion
+    }
+}
