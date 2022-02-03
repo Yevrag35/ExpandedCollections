1a020fdd739c8c68cd1a95d177da16205edf58dd
Mike Garvey <mike.garvey@yevrag35.com>
2022-02-03 09:17:55 -0500

    Bump Versions

--- MG.Collections.Wpf/Properties/AssemblyInfo.cs
+++ MG.Collections.Wpf/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Yevrag35, LLC.")]
 [assembly: AssemblyProduct("MG.Collections.Wpf")]
-[assembly: AssemblyCopyright("Copyright © 2021 Yevrag35, LLC.  All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2021-2022 Yevrag35, LLC.  All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
