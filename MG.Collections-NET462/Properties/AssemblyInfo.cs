26b8392f1d5e09696a7af48bd9ff135113fe6b94
Mike Garvey <mike.garvey@yevrag35.com>
2022-05-01 10:42:38 -0400

    Update Packages and References

--- MG.Collections-NET462/Properties/AssemblyInfo.cs
+++ MG.Collections-NET462/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MG.Collections-NET462")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MG.Collections")]
+[assembly: AssemblyDescription("A library consisting of specialized collection classes.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MG.Collections-NET462")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("Yevrag35, LLC.")]
+[assembly: AssemblyProduct("MG.Collections - .NET Framework")]
+[assembly: AssemblyCopyright("Copyright © 2021-2022 Yevrag35, LLC.  All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
